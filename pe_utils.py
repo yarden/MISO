06bae7451b9aaf6204a070c85a035994351f76de
yarden <yarden.katz@gmail.com>
2011-12-02 17:27:14 -0500

    Allow underscores in GFF files

--- pe_utils.py
+++ pe_utils.py
@@ -13,6 +13,7 @@ from numpy import *
 from parse_csv import *
 import Gene as gene_utils
 import sam_utils
+import exon_utils
 
 
 def load_insert_dist(insert_dist_filename):
@@ -24,18 +25,42 @@ def load_insert_dist(insert_dist_filename):
                          for line in insert_dist_file])
     return insert_dist
 
-    # insert_filename = glob.glob(os.path.join(insert_dir, "*.insert_dist"))
-    # if len(insert_filename) == 0:
-    #     raise Exception, "No insert length file found in %s." %(insert_dir)
 
+def bedtools_map_bam_to_bed(bam_filename, gff_intervals_filename):
+    """
+    Map BAM file to GFF intervals and return the result as a
+    BED file.
 
-def compute_insert_len(bam_filename, gff_filename, output_dir,
+    Returns a stream to a BED file with the results
+    """
+    bedtools_cmd = "intersectBed -abam %s -b %s -wa -wb -bed -f 1" \
+                   %(bam_filename, gff_intervals_filename)
+
+    print "Executing: %s" %(bedtools_cmd)
+
+    if (not os.path.isfile(bam_filename)) or \
+       (not os.path.isfile(gff_intervals_filename)):
+        raise Exception, "Error: %s or %s do not exist." %(bam_filename,
+                                                           gff_intervals_filename)
+    bed_stream = os.popen(bedtools_cmd)
+    return bed_stream 
+
+    
+def compute_insert_len(bams_to_process, gff_filename, output_dir,
                        min_exon_size):
     """
     Compute insert length distribution and output it to the given
     directory.
+
+    Arguments:
+
+    - bams_to_process: a list of BAM files to process
+    - gff_filename: GFF with gene models
     """
-    print "Computing insert length distribution of %s" %(bam_filename)
+    bams_str = "\n  ".join(bams_to_process)
+    num_bams = len(bams_to_process)
+    print "Computing insert length distribution of %d files:\n  %s" \
+          %(num_bams, bams_str)
     print "  - Using gene models from: %s" %(gff_filename)
     print "  - Outputting to: %s" %(output_dir)
     print "  - Minimum exon size used: %d" %(min_exon_size)
@@ -44,75 +69,160 @@ def compute_insert_len(bam_filename, gff_filename, output_dir,
         print "Making directory: %s" %(output_dir)
         os.makedirs(output_dir)
 
-    output_filename = os.path.join(output_dir,
-                                   "%s.insert_len" %(os.path.basename(bam_filename)))
+    # Get the constitutive exons that meet the size requirement
+    const_exons, gff_exons_filename = exon_utils.get_const_exons_by_gene(gff_filename, output_dir,
+                                                                         min_size=min_exon_size)
 
-    # Load BAM file with reads
-    bamfile = sam_utils.load_bam_reads(bam_filename)
+    for bam_filename in bams_to_process:
+        output_filename = os.path.join(output_dir,
+                                       "%s.insert_len" %(os.path.basename(bam_filename)))
+
+        # Load BAM file with reads
+        #bamfile = sam_utils.load_bam_reads(bam_filename)
+
+        t1 = time.time()
+        bed_mapped_reads = bedtools_map_bam_to_bed(bam_filename, gff_exons_filename)
+        t2 = time.time()
+
+        print "Fetching of reads in constitutive exons took: %.2f seconds" \
+              %(t1 - t2)
+
+        for mapped_read in bed_mapped_reads:
+            print "=> ", mapped_read
+
+        insert_lengths = []
+
+        t1 = time.time()
+
+        relevant_region = 0
     
-    # Load the genes from the GFF
-    print "Loading genes from GFF..."
-    t1 = time.time()
-    gff_genes = gene_utils.load_genes_from_gff(gff_filename)
-    t2 = time.time()
-    print "  - Loading genes from GFF took %.2f seconds" %(t2 - t1)
+        # paired_reads = sam_utils.pair_sam_reads(exon_reads)
+        # num_paired_reads = len(paired_reads)
 
-    insert_lengths = []
+        # if num_paired_reads == 0:
+        #     continue
+        # for read_pair_id, read_pair in paired_reads.iteritems():
+        #     if len(read_pair) != 2:
+        #         # Skip non-paired reads
+        #         continue
 
-    t1 = time.time()
+        #     left_read, right_read = read_pair
+        #     insert_len = right_read.pos - left_read.pos + 1
 
-    relevant_region = 0
+        #     if insert_len > 0:
+        #         insert_lengths.append(insert_len)
+        #     else:
+        #         print "Negative or zero insert length ignored..."
+
+        # Output results to file
+        #output_file = open(output_filename, 'w')
+        #insert_length_str = "\n".join(map(str, insert_lengths))
+        #output_file.write(insert_length_str)
+        #output_file.close()
+
+        t2 = time.time()
+        print "Insert length computation took %.2f seconds." %(t2 - t1)
+
+
+# def pair_reads_from_bed_intervals(bed_stream):
+#     """
+#     Match up read mates with each other, indexed by the BED interval
+#     that they fall in.
+
+#     Return a dictionary of BED region mapping to a set of read pairs.
+
+#     Arguments:
+
+#     - bed_filename: file with BED reads and the region they map to.
+
+#     Returns. 
+#     """
+#     return
+
+# def compute_insert_len(bam_filename, gff_filename, output_dir,
+#                        min_exon_size):
+#     """
+#     Compute insert length distribution and output it to the given
+#     directory.
+#     """
+#     print "Computing insert length distribution of %s" %(bam_filename)
+#     print "  - Using gene models from: %s" %(gff_filename)
+#     print "  - Outputting to: %s" %(output_dir)
+#     print "  - Minimum exon size used: %d" %(min_exon_size)
+
+#     if not os.path.isdir(output_dir):
+#         print "Making directory: %s" %(output_dir)
+#         os.makedirs(output_dir)
+
+#     output_filename = os.path.join(output_dir,
+#                                    "%s.insert_len" %(os.path.basename(bam_filename)))
+
+#     # Load BAM file with reads
+#     bamfile = sam_utils.load_bam_reads(bam_filename)
     
-    for gene_id, gene_info in gff_genes.iteritems():
-        gene_obj = gene_info["gene_object"]
+#     # Load the genes from the GFF
+#     print "Loading genes from GFF..."
+#     t1 = time.time()
+#     gff_genes = gene_utils.load_genes_from_gff(gff_filename)
+#     t2 = time.time()
+#     print "  - Loading genes from GFF took %.2f seconds" %(t2 - t1)
 
-        # Get all the constitutive parts
-        const_parts = gene_obj.get_const_parts()
+#     insert_lengths = []
 
-        chrom = gene_obj.chrom
+#     t1 = time.time()
 
-        # Consider only the large constitutive parts
-        for part in const_parts:
-            if part.len >= min_exon_size:
-                # Get all the reads that land in the coordinates of the exon
-                try:
-                    exon_reads = bamfile.fetch(chrom, part.start, part.end)
-                except ValueError:
-                    print "Could not fetch from region: ", chrom, part.start, part.end
-                    continue
+#     relevant_region = 0
+    
+#     for gene_id, gene_info in gff_genes.iteritems():
+#         gene_obj = gene_info["gene_object"]
 
-                # Pair all the paired-end reads that land there
-                paired_reads = sam_utils.pair_sam_reads(exon_reads)
-                num_paired_reads = len(paired_reads)
+#         # Get all the constitutive parts
+#         const_parts = gene_obj.get_const_parts()
 
-                if num_paired_reads == 0:
-                    continue
+#         chrom = gene_obj.chrom
 
-                print "Found %d region" %(relevant_region)
-                relevant_region += 1
+#         # Consider only the large constitutive parts
+#         for part in const_parts:
+#             if part.len >= min_exon_size:
+#                 # Get all the reads that land in the coordinates of the exon
+#                 try:
+#                     exon_reads = bamfile.fetch(chrom, part.start, part.end)
+#                 except ValueError:
+#                     print "Could not fetch from region: ", chrom, part.start, part.end
+#                     continue
 
-                # Compute the insert length of each read
-                for read_pair_id, read_pair in paired_reads.iteritems():
-                    if len(read_pair) != 2:
-                        # Skip non-paired reads
-                        continue
+#                 # Pair all the paired-end reads that land there
+#                 paired_reads = sam_utils.pair_sam_reads(exon_reads)
+#                 num_paired_reads = len(paired_reads)
+
+#                 if num_paired_reads == 0:
+#                     continue
+
+#                 print "Found %d region" %(relevant_region)
+#                 relevant_region += 1
+
+#                 # Compute the insert length of each read
+#                 for read_pair_id, read_pair in paired_reads.iteritems():
+#                     if len(read_pair) != 2:
+#                         # Skip non-paired reads
+#                         continue
                     
-                    left_read, right_read = read_pair
-                    insert_len = right_read.pos - left_read.pos + 1
+#                     left_read, right_read = read_pair
+#                     insert_len = right_read.pos - left_read.pos + 1
 
-                    if insert_len > 0:
-                        insert_lengths.append(insert_len)
-                    else:
-                        print "Negative or zero insert length ignored..."
+#                     if insert_len > 0:
+#                         insert_lengths.append(insert_len)
+#                     else:
+#                         print "Negative or zero insert length ignored..."
 
-    # Output results to file
-    output_file = open(output_filename, 'w')
-    insert_length_str = "\n".join(map(str, insert_lengths))
-    output_file.write(insert_length_str)
-    output_file.close()
+#     # Output results to file
+#     output_file = open(output_filename, 'w')
+#     insert_length_str = "\n".join(map(str, insert_lengths))
+#     output_file.write(insert_length_str)
+#     output_file.close()
                     
-    t2 = time.time()
-    print "Insert length computation took %.2f seconds." %(t2 - t1)
+#     t2 = time.time()
+#     print "Insert length computation took %.2f seconds." %(t2 - t1)
     
 
 def main():
@@ -120,19 +230,22 @@ def main():
     parser = OptionParser()
     parser.add_option("--compute-insert-len", dest="compute_insert_len", nargs=3, default=None,
                       help="Compute insert length for given sample. Takes as input "
-                      "a sorted, indexed BAM file with headers, a GFF file with gene models, "
-                      "and an output directory.")
+                      "(1) a comma-separated list of sorted, indexed BAM files with headers "
+                      "(or a single BAM filename), (2) a GFF file with gene models, "
+                      "and (3) an output directory.")
     parser.add_option("--min-exon-size", dest="min_exon_size", nargs=1, type="int", default=500,
                       help="Minimum size of constitutive exon (in nucleotides) that should be used "
-                      "in the computation.  Default is 500 bp.")
+                      "in the computation. Default is 500 bp.")
     (options, args) = parser.parse_args()
 
     if options.compute_insert_len != None:
-        bam_filename = os.path.abspath(os.path.expanduser(options.compute_insert_len[0]))
+        bams_to_process = [os.path.abspath(os.path.expanduser(f)) for f in \
+                           options.compute_insert_len[0].split(",")]
         gff_filename = os.path.abspath(os.path.expanduser(options.compute_insert_len[1]))
         output_dir = os.path.abspath(os.path.expanduser(options.compute_insert_len[2]))
 
-        compute_insert_len(bam_filename, gff_filename, output_dir, options.min_exon_size)
+        compute_insert_len(bams_to_process, gff_filename, output_dir,
+                           options.min_exon_size)
 
 
 if __name__ == "__main__":
--- plot_miso.py
+++ plot_miso.py
@@ -6,27 +6,60 @@ import os
 import matplotlib
 
 import pysam
+import shelve
 import GFF as gff_utils
 from plot_utils.samples_plotter import SamplesPlotter, load_samples
 from plot_utils.plotting import *
+from plot_utils.plot_gene import *
 import matplotlib.pyplot as plt
 from matplotlib import rc
-from plot_gene import *
 
-def plot_density(pickle_filename, bam_filename):
-    print "Plotting density along alternative isoform"
-    print "  - pickle: %s" %(pickle_filename)
-    print "  - BAM: %s" %(bam_filename)
-    gff_genes = gff_utils.load_indexed_gff_file(pickle_filename)
-    #bamfile = pysam.Samfile(bam_filename, "rb")
 
-    plot_gene(gff_genes)
+def plot_event(event_name, pickle_dir, settings_filename,
+               output_dir, png=False):
+    """
+    Visualize read densities across the exons and junctions
+    of a given MISO alternative RNA processing event.
 
-    plt.show()
+    Also plots MISO estimates and Psi values.
+    """
+    # Retrieve the full pickle filename
+    genes_filename = os.path.join(pickle_dir,
+                                  "genes_to_filenames.shelve")
 
-    print "gff_genes: ", gff_genes
+    if not os.path.isfile(genes_filename):
+        raise Exception, "Cannot find file %s. Are you sure the events " \
+              "were indexed with the latest version of index_gff.py?" \
+              %(genes_filename)
     
+    event_to_filenames = shelve.open(genes_filename)
+    if event_name not in event_to_filenames:
+        raise Exception, "Event %s not found in pickled directory %s. " \
+              "Are you sure this is the right directory for the event?" \
+              %(event_name, pickle_dir)
     
+    pickle_filename = event_to_filenames[event_name]
+
+    # Output filename is the event name
+    output_filename = os.path.join(output_dir, event_name)
+
+    # Determine format of output file
+    if png:
+        format_type = "png"
+    else:
+        format_type = "pdf"
+
+    output_filename += ".%s" %(format_type)
+        
+    print "Plotting read densities and MISO estimates along event..."
+    print "  - Event: %s" %(event_name)
+    print "  - Output filename: %s" %(output_filename)
+        
+    plot_density_from_file(pickle_filename, event_name,
+                           settings_filename,
+                           output_filename)
+
+
 def plot_posterior(miso_filename, output_dir,
                    with_intervals=None,
                    dimensions=None,
@@ -35,9 +68,6 @@ def plot_posterior(miso_filename, output_dir,
     """
     Plot posterior distribution.
     """
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-
     samples, log_scores, params, gene = load_samples(miso_filename)
     sp = SamplesPlotter(samples, gene, params)
     
@@ -75,6 +105,11 @@ def main():
     parser.add_option("--plot-posterior", dest="plot_posterior", nargs=1, default=None,
                       help="Plot the posterior distribution. Takes as input a raw MISO output "
                       "file (.miso)")
+    parser.add_option("--plot-event", dest="plot_event", nargs=3, default=None,
+                      help="Plot read densities and MISO inferences for a given alternative event. "
+                      "Takes the arguments: (1) event name (i.e. the ID= of the event based on MISO gff3 "
+                      "annotation file, (2) directory where the pickled annotation files are, "
+                      "(3) path to plotting settings file.")
     parser.add_option("--with-intervals", dest="with_intervals", nargs=1, default=None,
                       help="Include confidence intervals in plot. To be used with --plot-posterior. "
                       "Takes an argument between 1 and 99 corresponding to the confidence "
@@ -84,13 +119,9 @@ def main():
     parser.add_option("--dimensions", dest="dimensions", nargs=2, default=None,
                       help="Dimensions of the outputted figure: takes width by height (in inches).")
     parser.add_option("--png", dest="png", default=False, action="store_true",
-                      help="Output plot in PNG format (the default is PDF).")                      
+                      help="Output plot in PNG format (the default is PDF).")    
     parser.add_option("--output-dir", dest="output_dir", nargs=1, default=None,
                       help="Output directory.")
-    parser.add_option("--plot-density", dest="plot_density", nargs=2, default=None,
-                      help="Plot density of reads along an alternative event. Takes as "
-                      "argument a pickle filename corresponding to an event and a BAM "
-                      "along an alternative isoform event.")
     (options, args) = parser.parse_args()
 
     if options.output_dir == None:
@@ -98,20 +129,23 @@ def main():
         return
 
     output_dir = os.path.abspath(os.path.expanduser(options.output_dir))
+    dimensions = None
+
+    if options.dimensions != None:
+        dimensions = (int(options.dimensions[0]),
+                      int(options.dimensions[1]))
+
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
 
     if options.plot_posterior != None:
         miso_filename = os.path.abspath(os.path.expanduser(options.plot_posterior))
 
         with_intervals = None
-        dimensions = None
         plot_mean = options.plot_mean
         
         if options.with_intervals != None:
             with_intervals = options.with_intervals[0]
-
-        if options.dimensions != None:
-            dimensions = (int(options.dimensions[0]),
-                          int(options.dimensions[1]))
 
         plot_posterior(miso_filename, output_dir,
                        with_intervals=with_intervals,
@@ -119,10 +153,17 @@ def main():
                        plot_mean=plot_mean,
                        png=options.png)
 
-    if options.plot_density != None:
         pickle_filename = os.path.abspath(os.path.expanduser(options.plot_density[0]))
         bam_filename = os.path.abspath(os.path.expanduser(options.plot_density[1]))
         plot_density(pickle_filename, bam_filename)
+
+    if options.plot_event != None:
+        # Plot a MISO event along with its RNA-Seq read densities
+        event_name = options.plot_event[0]
+        pickle_dir = os.path.abspath(os.path.expanduser(options.plot_event[1]))
+        settings_filename = os.path.abspath(os.path.expanduser(options.plot_event[2]))
+        plot_event(event_name, pickle_dir, settings_filename, output_dir,
+                   png=options.png)
         
 
 if __name__ == '__main__':
