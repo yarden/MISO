cc6dd3aa0d381555dcf8c6e2cdba4f09f1f7bb84
yarden <yarden.katz@gmail.com>
2011-12-03 14:50:28 -0500

    revised sam utils

--- sam_utils.py
+++ sam_utils.py
@@ -5,6 +5,7 @@
 from collections import defaultdict
 from Gene import load_genes_from_gff
 
+import os
 import time
 import pysam
 import binascii
@@ -166,6 +167,7 @@ def load_bam_reads(bam_filename,
     Load a set of indexed BAM reads.
     """
     print "Loading BAM filename from: %s" %(bam_filename)
+    bam_filename = os.path.abspath(os.path.expanduser(bam_filename))
     t1 = time.time()
     bamfile = pysam.Samfile(bam_filename, "rb",
                             template=template)
