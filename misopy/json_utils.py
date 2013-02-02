fd9d0ac829580f3494098d59ad41d2302fa47327
Yarden Katz <yarden.katz@gmail.com>
2013-02-02 16:27:12 -0500

    simplejson back into requirements

--- misopy/json_utils.py
+++ misopy/json_utils.py
@@ -1,14 +1,17 @@
 import os
-import simplejson
+try:
+    import simplejson as json
+except:
+    import json 
 
 def json_serialize(obj, filename):
     f = open(filename, 'w')
-    simplejson.dump(obj, f, indent=1)	
+    json.dump(obj, f, indent=1)	
     f.close()
 
 def json_load_file(filename):
     if os.path.isdir(filename):
 	raise Exception, "%s is a directory -- expected a JSON filename." %(filename)
     f = open(filename)
-    obj = simplejson.load(f)
+    obj = json.load(f)
     return obj
