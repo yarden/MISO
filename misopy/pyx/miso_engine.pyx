152c249d4bc0e4a9388c68d311f8b7f1e18cdbf7
Yarden Katz <yarden.katz@gmail.com>
2014-03-02 01:07:08 -0500

    cleanup

--- misopy/pyx/miso_engine.pyx
+++ misopy/pyx/miso_engine.pyx
@@ -6,22 +6,21 @@
 cimport numpy as np
 import numpy as np
 
-import misopy
-import misopy.matrix_utils as matrix_utils
-
-ctypedef np.int_t DTYPE_t
-ctypedef np.float_t DTYPE_float_t
+cimport matrix_utils
+cimport array_utils
+cimport math_utils
+cimport sampling_utils
 
 
 cdef class MISOEngine_PairedEnd:
   """
   MISO paired-end engine.
   """
-  cdef np.ndarray[DTYPE_t, ndim=2] reads
+  cdef int[:, :] reads
 
   
 cdef class MISOEngine_SingleEnd:
   """
   MISO single-end engine.
   """
-  cdef np.ndarray[DTYPE_t, ndim=2] reads
+  cdef [:, :] reads
