46f01ad80b446aeffd2503882d3510e5a8b20562
Gabor Csardi <csardi.gabor@gmail.com>
2015-01-12 11:25:27 -0500

    Tests check generated reads and MISO results

--- tests/test_two_iso_gene.py
+++ tests/test_two_iso_gene.py
@@ -5,6 +5,9 @@ import misopy.gff_utils
 import pysplicing
 import random
 
+from scipy import *
+from numpy import *
+
 gene = pysplicing.createGene( ((1, 100), (201, 300), (401, 500)),
                               ((0, 1, 2), (0, 2)) )
 
@@ -17,10 +20,14 @@ def test_isoLength():
 def test_simulate_reads():
     random.seed(42)
     reads = pysplicing.simulateReads(gene, 0L, (0.2, 0.8), 2000L, 33L)
+    assert(mean(reads[0]) == 0.7185)
 
 def test_MISO():
     random.seed(42)
     reads = pysplicing.simulateReads(gene, 0L, (0.2, 0.8), 2000L, 33L)
     random.seed(42)
-    results = pysplicing.MISO(gene, 0L, reads[1], reads[2], 33L, 50L,
-                              10L, 2L, (1.0, 1.0))
+    results = pysplicing.MISO(gene, 0L, reads[1], reads[2], 33L, 500L,
+                              100L, 10L, (1.0, 1.0))
+
+    psi = transpose(array(results[0]))
+    assert(all(mean(psi, 0) - [ 0.19308989, 0.80691011] < 1e-8))
