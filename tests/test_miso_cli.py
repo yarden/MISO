54dcd9ca805d32eb4dddc358699f5dec9f857835
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-11 14:29:11 -0400

    Smaller GFF for tests, #10
    It takes ~17 seconds now, much better, but still way
    too long.

--- tests/test_miso_cli.py
+++ tests/test_miso_cli.py
@@ -1,32 +1,35 @@
 
 import os
 import sys
-import tempfile
 import shutil
 
 from nose.tools import *
 
-import setup_miso
-
 def miso_cli(sams = None, gff = None):
 
-    miso_dir = setup_miso.miso_dir
+    import misopy
+    miso_dir = misopy.__path__[0]
 
-    # Data files in a temporary directory
+    # Data files to test directory, if the output directory exists,
+    # then we assume that it has these files already.
     if gff is None:
-        test_gff = os.path.join(miso_dir, "misopy", "gff-events", "mm9",
-                                "SE.mm9.gff")
+        test_gff = os.path.join(miso_dir, "..", "tests", "test.gff")
     if sams is None:
-        sams = [ os.path.join(miso_dir, "misopy", "test-data", "sam-data",
+        sams = [ os.path.join(miso_dir, "test-data", "sam-data",
                              "c2c12.Atp2b1.sam") ]
 
-    dirpath = tempfile.mkdtemp()
+    dirpath = 'output'
+    try:
+        os.mkdir(dirpath)
+    except OSError:
+        None
+
     os.chdir(dirpath)
     shutil.copyfile(test_gff, os.path.basename(test_gff))
     [ shutil.copyfile(sam, os.path.basename(sam)) for sam in sams ]
 
     # SAM to BAM
-    import sam_to_bam
+    from misopy import sam_to_bam
     def tobam(sam):
         sam_to_bam.main(["--convert", os.path.basename(sam), "my_sample"])
         return os.path.join(
@@ -36,31 +39,30 @@ def miso_cli(sams = None, gff = None):
     bams = [ tobam(sam) for sam in sams ]
 
     # Index GFF
-    import index_gff
+    from misopy import index_gff
     index_gff.main(["--index", os.path.basename(test_gff), "indexed"])
 
     # Run MISO
-    import miso
+    from misopy import miso
     miso_args = ["--run", "indexed"] + bams + \
                 ["--output-dir", "output", "--read-len", "36"]
     miso.main(miso_args)
 
     # Summarize
-    import summarize_miso
+    from misopy import summarize_miso
     summarize_miso.main(["--summarize-samples", "output", "."])
-
-    # Clean up
-    shutil.rmtree(dirpath)
 
 def test_miso_cli():
     miso_cli()
 
 @raises(SystemExit)
 def test_error_if_multiple_bam_files():
-    miso_dir = setup_miso.miso_dir
-    test_sam = os.path.join(miso_dir, "misopy", "test-data", "sam-data",
+    import misopy
+    miso_dir = misopy.__path__[0]
+    test_sam = os.path.join(miso_dir, "test-data", "sam-data",
                             "c2c12.Atp2b1.sam")
-    test_sam2 = os.path.dirname(test_sam) + "c2c12.Atp2b2-2.sam"
+    test_sam2 = os.path.join(os.path.dirname(test_sam),
+                             "c2c12.Atp2b1-2.sam")
     shutil.copyfile(test_sam, test_sam2)
 
     miso_cli(sams = [test_sam, test_sam2])
