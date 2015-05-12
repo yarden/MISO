655e2eef0e38f4ec0e5080baf0c6849df0b5560f
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-12 11:01:26 -0400

    Proper test case for logistic prior

--- tests/test_logistic_prior.py
+++ tests/test_logistic_prior.py
@@ -3,7 +3,7 @@ import setup_miso
 import misopy
 import misopy.gff_utils
 import pysplicing
-import random
+import random as plainrandom
 
 from scipy import *
 from numpy import *
@@ -12,14 +12,18 @@ gene = pysplicing.createGene( ((1, 100), (201, 300), (401, 500)),
                               ((0, 1, 2), (0, 2)) )
 
 def test_MISO():
+    plainrandom.seed(42)
     reads = pysplicing.simulateReads(gene, 0L, (0.5, 0.5), 10L, 33L)
 
     results1 = pysplicing.doMISO(
         gene, 0L, (tuple(reads[1:3]),), 33L, 5000L, 1000L, 10L)
     psi1 = transpose(array(results1[0]))
+    meanpsi1 = mean(psi1, 0)
 
-    print
-    print "Dirichlet:", mean(psi1, 0)
+    print "Dirichlet:", meanpsi1
+
+    assert( abs(meanpsi1[0] - 0.43982318) < 1e-8 and
+            abs(meanpsi1[1] - 0.56017682) < 1e-8 )
 
     results2 = pysplicing.doMISO(
         GFF = gene, gene = 0L, reads = (tuple(reads[1:3]),),
@@ -27,8 +31,12 @@ def test_MISO():
         prior = pysplicing.MISO_PRIOR_LOGISTIC, logistic_prior_mean = 0.0,
         logistic_prior_var = .01)
     psi2 = transpose(array(results2[0]))
+    meanpsi2 = mean(psi2, 0)
 
-    print "Logistic: ", mean(psi2, 0)
+    print "Logistic: ", meanpsi2
+
+    assert( abs(meanpsi2[0] - 0.49930664) < 1e-8 and
+            abs(meanpsi2[1] - 0.50069336) < 1e-8 )
 
     results3 = pysplicing.doMISO(
         GFF = gene, gene = 0L, reads = (tuple(reads[1:3]),),
@@ -36,10 +44,15 @@ def test_MISO():
         prior = pysplicing.MISO_PRIOR_LOGISTIC, logistic_prior_mean = 5.0,
         logistic_prior_var = 1.0)
     psi3 = transpose(array(results3[0]))
+    meanpsi3 = mean(psi3, 0)
 
-    print "Logistic: ", mean(psi3, 0)
+    print "Logistic: ", meanpsi3
+
+    assert( abs(meanpsi3[0] - 0.92413627) < 1e-8 and
+            abs(meanpsi3[1] - 0.07586373) < 1e-8 )
 
 def test_paired_MISO():
+    plainrandom.seed(42)
     reads = pysplicing.simulatePairedReads(gene,       # gff
                                            0L,         # gene
                                            (0.5, 0.5), # expression
@@ -52,9 +65,12 @@ def test_paired_MISO():
     results1 = pysplicing.doMISOPaired(
         gene, 0L, (tuple(reads[1:3]),), 33L, 60, 5, 4, 5000L, 1000L, 10L)
     psi1 = transpose(array(results1[0]))
+    meanpsi1 = mean(psi1, 0)
 
-    print
-    print "Dirichlet:", mean(psi1, 0)
+    print "Dirichlet:", meanpsi1
+
+    assert( abs(meanpsi1[0] - 0.36982598) < 1e-8 and
+            abs(meanpsi1[1] - 0.63017402) < 1e-8 )
 
     results2 = pysplicing.doMISOPaired(
         GFF = gene, gene = 0L, reads = (tuple(reads[1:3]),),
@@ -63,8 +79,12 @@ def test_paired_MISO():
         prior = pysplicing.MISO_PRIOR_LOGISTIC, logistic_prior_mean = 0.0,
         logistic_prior_var = 0.01)
     psi2 = transpose(array(results2[0]))
+    meanpsi2 = mean(psi2, 0)
 
-    print "Logistic: ", mean(psi2, 0)
+    print "Logistic: ", meanpsi2
+
+    assert( abs(meanpsi2[0] - 0.39687208) < 1e-8 and
+            abs(meanpsi2[1] - 0.60312792) < 1e-8 )
 
     results3 = pysplicing.doMISOPaired(
         GFF = gene, gene = 0L, reads = (tuple(reads[1:3]),),
@@ -73,8 +93,12 @@ def test_paired_MISO():
         prior = pysplicing.MISO_PRIOR_LOGISTIC, logistic_prior_mean = 5.0,
         logistic_prior_var = 1.0)
     psi3 = transpose(array(results3[0]))
+    meanpsi3 = mean(psi3, 0)
 
-    print "Logistic: ", mean(psi3, 0)
+    print "Logistic: ", meanpsi3
+
+    assert( abs(meanpsi3[0] - 0.39861373) < 1e-8 and
+            abs(meanpsi3[1] - 0.60138627) < 1e-8 )
 
 if __name__ == "__main__":
     test_MISO()
