3dd4f70c70bfeb8af1a43a1488e7579b5a890995
BaileyGlen <baileyglen@gmail.com>
2017-08-16 10:35:15 -0400

    fixing samtools sort output
    Samtools sort was missing the -o argument, which didn't work with current versions of samtools. I believe this fix will work starting with samtools 1.0 forward. May not work with earlier samtools. Could add in a samtools version check.

--- misopy/sam_to_bam.py
+++ misopy/sam_to_bam.py
@@ -25,8 +25,8 @@ def sam_to_bam(sam_filename, output_dir,
     # Sort
     print "Sorting BAM file..."
     sorted_filename = "%s.sorted" %(bam_filename.split(".bam")[0])
-    cmd = "samtools sort %s %s" %(bam_filename,
-                                  sorted_filename)
+    cmd = "samtools sort -o %s %s" %(sorted_filename,
+                                     bam_filename)
     print "  - Executing: %s" %(cmd)
     os.system(cmd)
 
